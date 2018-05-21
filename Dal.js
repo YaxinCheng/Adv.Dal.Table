ce4d0d00e5d5e1b6970aadd7c239436eac127303
Yaxin Cheng <yaxin.cheng@dal.ca>
2018-05-20 22:36:49 -0300

    Initial version

---
+++ Dal.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         Dalhousie Advanced Academic Timetable
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Easier course selection
+// @author       Yaxin Cheng
+// @match        https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term=*&s_subj=*&s_district=*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+function setupTheButton() {
+    let nextButton = document.createElement('button');
+    let buttonStyle = `
+        position: fixed;
+        right: 15px;
+        z-index: 100;
+        bottom: 10px;
+        width: 50px;
+        height: 50px;
+        background-color: #ffbf00;
+        border: none;
+        border-radius: 50%
+    `
+    nextButton.className = "nextButton";
+    nextButton.style = buttonStyle;
+    nextButton.innerHTML = "Next";
+    document.body.appendChild(nextButton);
+}
+
+function checkboxSelected(element) {
+    var crn = element.children[1].innerHTML;
+    var isChecked = element.getElementsByTagName('input')[0].checked;
+    if (isChecked) {
+        selected[crn] = true;
+    } else {
+        delete selected[crn];
+    }
+}
+
+function setupCheckbox() {
+    let rows = Array.from(document.querySelectorAll('table.dataentrytable > tbody > tr:not([valign=middle])')).slice(3);
+    let courseRows = rows.filter(function (tr) {
+        return tr.children[0].innerHTML != "<b>NOTE</b>";
+    });
+    courseRows.forEach(function(tr) {
+        tr.children[0].innerHTML = "<input type=checkbox> " + tr.children[0].innerHTML;
+        tr.children[0].addEventListener("click", function() { checkboxSelected(tr); }, false);
+    });
+}
+
+var selected = {};
+setupTheButton();
+setupCheckbox();
